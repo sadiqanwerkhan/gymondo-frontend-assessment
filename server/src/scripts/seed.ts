eefebc7341c7154a2c082a2a0c4406e36abc9603
Muhammad Sadiq Anwer Khan <Muhammad Sadiq Anwer Khan>
2025-05-12 04:36:01 +0200

    dataenhancement: populated the description to meet the size of ~10KB

--- server/src/scripts/seed.ts
+++ server/src/scripts/seed.ts
@@ -7,7 +7,7 @@ const categories = ["c1", "c2", "c3", "c4", "c5", "c6", "c7"];
 const generateWorkout = (id: number) => ({
   id: id.toString(),
   name: faker.lorem.words(3),
-  description: faker.lorem.paragraphs(3),
+  description: faker.lorem.paragraphs(15),
   startDate: faker.date.soon({ days: 365 }),
   category: faker.helpers.arrayElement(categories),
 });
